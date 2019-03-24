60e0e09508809b31bdd198a2773f09b2956da68d
Julian Lankstead <Julian@Julians-MacBook-Pro.local>
2019-03-24 15:05:38 -0400

    jquery functionality added. data now is received from the api, not the html hard code.

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ var app = express();
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
+var data = {
+  nodes: [
+    {id: "One", label: "1 "},
+    {id: "Two", label: " 2"},
+    {id: "Three", label: "3 "},
+    {id: "Four", label: " 4"},
+    {id: "Eight", label: " 8"},
+    {id: "Six", label: " 6"}
+  ],
+  edges: [],
+  options: {}
+};
+app.use("/api", function(req, res){
+  res.send(data).end();
+
+});
+
 app.listen(8080, function(){
   console.log("Your server was stated on port 8080");
 });
