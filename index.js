49c787c66b35c25eee1acc5034770e3cca7abd55
Julian Lankstead <Julian@Julians-MacBook-Pro.local>
2019-03-24 14:51:35 -0400

    Nodes added and vis.js implemented

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello Wors");
+var express = require("express");
+var app = express();
+
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+
+app.listen(8080, function(){
+  console.log("Your server was stated on port 8080");
+});
